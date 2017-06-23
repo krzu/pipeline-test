cc66ef3cdcaacfb46b4e63be85b62b66fd328d5d
Krystian Zubrycki <kzubrycki@avaya.com>
2017-06-23 14:52:13 +0200

    added android license

--- ci/build.sh
+++ ci/build.sh
@@ -2,5 +2,6 @@
 
 export TERM=${TERM:-dumb}
 export ANDROID_HOME="$PWD/android-sdk-linux"
+echo -e "\n8933bad161af4178b1185d1a37fbf41ea5269c55" > "$ANDROID_HOME/licenses/android-sdk-license"
 cd project-source
 ./gradlew build
