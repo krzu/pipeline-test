54c86c02e58480bd640a48c361214af48c887244
Krystian Zubrycki <kzubrycki@avaya.com>
2017-06-27 10:43:58 +0200

    using 'chmod +x' instead of 'chmod 777'

--- ci/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -ex
-export TERM=${TERM:-dumb}
-export ANDROID_HOME="$PWD/android-sdk-linux"
-mkdir "$ANDROID_HOME/licenses"
-echo -e "\n8933bad161af4178b1185d1a37fbf41ea5269c55" > "$ANDROID_HOME/licenses/android-sdk-license"
-cd project-source
-./gradlew build
